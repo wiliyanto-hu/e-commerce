5fe7ad1e3543571b9aa783ae78505ea42eb117df
rottenkitkat <williyantohuang97@yahoo.com>
2021-04-16 10:55:58 +0700

    changed category function

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -8,30 +8,38 @@ const ProductList = ({ addItem, category }) => {
   const [products, setProducts] = React.useState([]);
   React.useEffect(() => {
     async function fetchData() {
-      let response;
-      if (!category || category === "all products") {
-        response = await axios.get("https://fakestoreapi.com/products");
-      } else {
-        response = await axios.get(
-          `https://fakestoreapi.com/products/category/${category}`
-        );
-      }
+      const response = await axios.get("https://fakestoreapi.com/products");
       const { data } = response;
       setProducts(data);
     }
     fetchData();
-  }, [category]);
+  }, []);
 
+  const allProducts = () => {
+    if (category === "all products" || !category) {
+      return products.map((p) => (
+        <Product product={p} key={p.id} addItem={addItem} />
+      ));
+    } else {
+      const filtered = products.filter((p) => p.category === category);
+      return filtered.map((p) => (
+        <Product product={p} key={p.id} addItem={addItem} />
+      ));
+    }
+  };
   return (
     <Grid container spacing={6}>
-      {products.length ? (
-        products.map((p) => (
-          <Product product={p} key={p.id} addItem={addItem} />
-        ))
-      ) : (
-        <Spinner />
-      )}
+      {products.length ? allProducts() : <Spinner />}
     </Grid>
   );
 };
 export default ProductList;
+
+// {products.length ? (
+//   if(category)
+//   products.map((p) => (
+//     <Product product={p} key={p.id} addItem={addItem} />
+//   ))
+// ) : (
+//   <Spinner />
+// )}
